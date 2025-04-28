281cff4d13f826b5909876daf50dedbc73bb96d2
b3rian <brayann.8189@gmail.com>
2025-04-28 09:15:32 +0300

    edited

--- database.py
+++ database.py
@@ -1,8 +1,14 @@
-def change_list(mylist):
-    mylist.append(4)
-    print('inside function:', mylist)
+def greetings(name, age):
+    """
+    Function to greet a user with their name and age.
+    
+    Parameters:
+    name (str): The name of the user.
+    age (int): The age of the user.
+    
+    Returns:
+    str: A greeting message.
+    """
+    print(f"Hello {name}, you are {age} years old!")
 
-
-a = [1, 2, 3]
-change_list(a)    
-print('outside function:', a)
+greetings('Alice', 25)
